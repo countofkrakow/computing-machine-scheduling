d7bc6e4b52754734f19baa9611bdf984999e9979
countofkrakow <chrisnakovski@gmail.com>
2016-05-15 13:44:28 -0700

    Added parser, sample input, and bare code file

---
+++ data.py
@@ -0,0 +1,34 @@
+# parses input
+
+filename = "input.txt"
+
+def parseFile(name=filename):
+    cases = []
+    f = open(name , 'r')
+    line = f.readline()
+    while line:
+        line_arr = line.split()
+
+        # add an empty case to the cases list
+        if len(line_arr) == 3:
+            case = {'machines': []}
+            case['N'] = int(line_arr[0])
+            case['C'] = int(line_arr[1])
+            case['D'] = int(line_arr[2])
+            cases.append(case)
+
+        # append a machine to the most recent case
+        elif len(line_arr) == 4:
+            machine = {
+                'day': int(line_arr[0]),
+                'purchase_price': int(line_arr[1]),
+                'resale_price': int(line_arr[2]),
+                'profit_rate': int(line_arr[3])
+            }
+            cases[-1]['machines'].append(machine)
+        else:
+            print('There was an error parsing the input file. Exiting...')
+            exit(1)
+
+        line = f.readline()
+    return cases
